73dc8d2e4f30518e0a4512e47fbf3c0660bdc909
gustavo machado <tadeboa793@gmail.com>
2026-09-04 16:55:57 -0300

    feat: estrutura de pastas, componentes e rotas da etapa 4

--- Meu-portifolio/meu-portfolio/src/app/app.routes.ts
+++ Meu-portifolio/meu-portfolio/src/app/app.routes.ts
@@ -1,3 +1,72 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'sobre',
+    loadComponent: () =>
+      import('./features/sobre/sobre').then(
+        m => m.Sobre
+      )
+  },
+
+  {
+    path: 'objetivo-profissional',
+    loadComponent: () =>
+      import('./features/objetivo-profissional/objetivo-profissional').then(
+        m => m.ObjetivoProfissional
+      )
+  },
+
+  {
+    path: 'formacao-academica',
+    loadComponent: () =>
+      import('./features/formacao-academica/formacao-academica').then(
+        m => m.FormacaoAcademica
+      )
+  },
+
+  {
+    path: 'habilidades',
+    loadComponent: () =>
+      import('./features/habilidades/habilidades').then(
+        m => m.Habilidades
+      )
+  },
+
+  {
+    path: 'certificacoes',
+    loadComponent: () =>
+      import('./features/certificacoes/certificacoes').then(
+        m => m.Certificacoes
+      )
+  },
+
+  {
+    path: 'qualificacoes',
+    loadComponent: () =>
+      import('./features/qualificacoes/qualificacoes').then(
+        m => m.Qualificacoes
+      )
+  },
+
+  {
+    path: 'projetos',
+    loadComponent: () =>
+      import('./features/projetos/projetos').then(
+        m => m.Projetos
+      )
+  },
+
+  // Redirecionamento inicial
+  {
+    path: '',
+    redirectTo: 'sobre',
+    pathMatch: 'full'
+  },
+
+  // Página não encontrada
+  {
+    path: '**',
+    redirectTo: 'sobre'
+  }
+];
