3369715a51eb035e1a76836eb9f983ffe8fa4f89
gustavo machado <tadeboa793@gmail.com>
2026-09-04 17:25:39 -0300

    feat: estilização do header e da tela sobre mim conforme prototipo

--- Meu-portifolio/meu-portfolio/src/app/shared/navbar/navbar.ts
+++ Meu-portifolio/meu-portfolio/src/app/shared/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive,MatButtonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
